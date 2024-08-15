0c605d1ccd66a6565a6075ee8873815dec210428
Sahil Gupta <ask.sahilgupta@gmail.com>
2024-08-16 00:46:00 +0530

    segregrated the skills in skills section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -32,73 +32,32 @@ function Technologies() {
   return (
     <div className='border-b border-neutral-800 pb-24'>
       <motion.h2 
-      initial={{opacity: 0, y: 100}}
-      whileInView={{opacity: 1, y: 0}}
+      initial={{opacity: 0, y: 100, filter:"blur(15px)"}}
+      whileInView={{opacity: 1, y: 0, filter: "blur(0px)"}}
       transition={{duration: 1.5}}
 
-      className='my-20 text-center text-4xl'>Technical Skills</motion.h2>
+      className='my-20 text-center text-4xl'>Skills</motion.h2>
+
+      <motion.h2 
+        initial={{opacity: 0, x: 100, filter:"blur(15px)"}}
+        whileInView={{opacity: 1, x: 0, filter: "blur(0px)"}}
+        transition={{duration: 1.5}}
+
+        className='my-20 text-center text-2xl text-neutral-400'>Frontend
+      </motion.h2>
+
       <motion.div 
-      initial={{opacity: 0, x: -100}}
-      whileInView={{opacity: 1, x: 0}}
+      initial={{opacity: 0, x: -100, filter:"blur(15px)"}}
+      whileInView={{opacity: 1, x: 0, filter: "blur(0px)"}}
       transition={{duration: 1.5}}
       className="flex flex-wrap items-center justify-center gap-4">
-        <motion.div 
         
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
 
-        className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiReactjsLine className='text-7xl text-cyan-400'/>
-        </motion.div>
-        <motion.div 
+
+
         
-        variants={iconVariants(3)}
-        initial="initial"
-        animate="animate"
 
-        className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaNodeJs className='text-7xl text-green-600'/>
-        </motion.div>
-        <motion.div 
         
-        variants={iconVariants(3.5)}
-        initial="initial"
-        animate="animate"
-
-        className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiExpress className='text-7xl text-gray-400'/>
-        </motion.div>
-        
-        <motion.div 
-        
-        variants={iconVariants(4)}
-        initial="initial"
-        animate="animate"
-
-        className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMongodb className='text-7xl text-green-700'/>
-        </motion.div>
-
-        <motion.div 
-        
-          variants={iconVariants(4.5)}
-          initial="initial"
-          animate="animate"
-
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-              <SiGit className='text-7xl text-white/85'/>
-        </motion.div>
-
-        <motion.div 
-        
-          variants={iconVariants(5)}
-          initial="initial"
-          animate="animate"
-
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-              <SiJavascript className='text-7xl text-yellow-400'/>
-        </motion.div>
 
         <motion.div 
         
@@ -120,6 +79,28 @@ function Technologies() {
               <SiCss3 className='text-7xl text-[#2929af]'/>
         </motion.div>
 
+        
+
+        <motion.div 
+        
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+              <SiJavascript className='text-7xl text-yellow-400'/>
+        </motion.div>
+
+        <motion.div 
+        
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+
+        className="rounded-2xl border-4 border-neutral-800 p-4">
+            <RiReactjsLine className='text-7xl text-cyan-400'/>
+        </motion.div>
+
         <motion.div 
         
           variants={iconVariants(6.5)}
@@ -132,12 +113,71 @@ function Technologies() {
 
         <motion.div 
         
+          variants={iconVariants(4.5)}
+          initial="initial"
+          animate="animate"
+
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+              <SiGit className='text-7xl text-orange-600'/>
+        </motion.div>
+        
+      </motion.div>
+
+      <motion.h2 
+        initial={{opacity: 0, x: 100, filter:"blur(15px)"}}
+        whileInView={{opacity: 1, x: 0, filter: "blur(0px)"}}
+        transition={{duration: 1.5}}
+
+        className='my-20 text-center text-2xl text-neutral-400'>Backend 
+      </motion.h2>
+
+      <motion.div 
+      initial={{opacity: 0, x: -100, filter:"blur(15px)"}}
+      whileInView={{opacity: 1, x: 0, filter: "blur(0px)"}}
+      transition={{duration: 1.5}}
+      className="flex flex-wrap items-center justify-center gap-4">
+        
+
+        <motion.div 
+        
+        variants={iconVariants(3)}
+        initial="initial"
+        animate="animate"
+
+        className="rounded-2xl border-4 border-neutral-800 p-4">
+            <FaNodeJs className='text-7xl text-green-600'/>
+        </motion.div>
+
+        <motion.div 
+        
+        variants={iconVariants(3.5)}
+        initial="initial"
+        animate="animate"
+
+        className="rounded-2xl border-4 border-neutral-800 p-4">
+            <SiExpress className='text-7xl text-gray-400'/>
+        </motion.div>
+        
+        <motion.div 
+        
+        variants={iconVariants(4)}
+        initial="initial"
+        animate="animate"
+
+        className="rounded-2xl border-4 border-neutral-800 p-4">
+            <SiMongodb className='text-7xl text-green-700'/>
+        </motion.div>
+
+        
+
+        <motion.div 
+        
           variants={iconVariants(7)}
           initial="initial"
           animate="animate"
 
           className="rounded-2xl border-4 border-neutral-800 p-4">
-              <SiPostman className='text-7xl text-orange-600'/>
+              <SiPostman className='text-7xl text-orange-700'/>
         </motion.div>
       </motion.div>
 
